b4595cd1009b81c03da617dc1163beec300b15eb
liuchangqing <1005428477@qq.com>
2015-07-29 21:07:34 +0800

    Signed-off-by: liuchangqing <1005428477@qq.com>

--- src/main/java/com/lcq/servlet/PrimitiveServlet.java
+++ src/main/java/com/lcq/servlet/PrimitiveServlet.java
@@ -1,23 +1,31 @@
 package com.lcq.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 public class PrimitiveServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public PrimitiveServlet() {
-        super();
-    }
+	private static final Logger logger = Logger
+			.getLogger(PrimitiveServlet.class);
 
-	public void init(ServletConfig config) throws ServletException {
+	public PrimitiveServlet() {
+		super();
+		logger.info(this.getServletName() + "初始化");
 	}
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("----------call init method----------");
+	}
+
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 	}
 
 }
