4c7d9492f29d13f8241e9378c53cd0a549e8fe63
liuchangqing <1005428477@qq.com>
2015-07-29 18:28:58 +0800

    新建PrimitiveServlet
    Signed-off-by: liuchangqing <1005428477@qq.com>

---
+++ src/main/java/com/lcq/servlet/PrimitiveServlet.java
@@ -0,0 +1,23 @@
+package com.lcq.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PrimitiveServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public PrimitiveServlet() {
+        super();
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
