376bb8dd11bae59e2edcd9e022a1bd88739191bf
liuchangqing <1005428477@qq.com>
2015-07-29 21:12:00 +0800

    Signed-off-by: liuchangqing <1005428477@qq.com>

--- src/main/java/com/lcq/constant/Constants.java
+++ src/main/java/com/lcq/constant/Constants.java
@@ -1,5 +1,7 @@
 package com.lcq.constant;
 
+import java.io.File;
+
 public class Constants {
-	public static final String WEB_ROOT="";
+	public static final String WEB_ROOT=System.getProperty("user.dir")+File.separator+"webroot";
 }
